df5994c8a8b017f1efac5c17acea3e251f299a66
Rameez Alam <rameez.gce@gmail.com>
2015-02-28 21:02:11 +0530

    Added factorial function

--- CoreJava/src/com/rameez/basic/HelloWorld.java
+++ CoreJava/src/com/rameez/basic/HelloWorld.java
@@ -4,6 +4,23 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+
+		//Added By Zeeshan	
+		int input= 25;		
+		int fact = factorial(input);
+		System.out.println("Factorial of "+input+" is : "+fact);
+	}
+
+	/**
+	 * This method will calculate the factorial of a number
+	 * @param n
+	 * @return
+	 */
+	private static int factorial(int n) {		
+		if(n == 1){
+			return 1;
+		}
+		return factorial(n-1)*n;
 	}
 
 }
